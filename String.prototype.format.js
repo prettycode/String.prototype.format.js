bc0f4d6a96e48fe2e508dfbbe661b481a19cf677
Chris O. <chris@prettycode.org>
2011-03-03 11:29:09 -0800

    Edited String.prototype.format.js via GitHub

--- String.prototype.format.js
+++ String.prototype.format.js
@@ -1,14 +1,14 @@
 /*
-	Original script title: String.prototype.format.js, v1.0
-	Copyright (c) 2011, Chris O'Brien, prettycode.org
-	http://github.com/prettycode/String.prototype.format.js
-	
-	LICENSE: Permission is hereby granted for unrestricted use,
-	modification, and redistribution of this script, ONLY under
-	the condition that this code comment is kept wholly complete,
-	appearing above the script's code body--in all original or
-	modified implementations of this script, except those that
-	are minified.
+    Original script title: String.prototype.format.js, v1.0
+    Copyright (c) 2011, Chris O'Brien, prettycode.org
+    http://github.com/prettycode/String.prototype.format.js
+    
+    LICENSE: Permission is hereby granted for unrestricted use,
+    modification, and redistribution of this script, ONLY under
+    the condition that this code comment is kept wholly complete,
+    appearing above the script's code body--in all original or
+    modified implementations of this script, except those that
+    are minified.
 */ 
 
 String.prototype.$format = function() {
@@ -26,19 +26,18 @@ String.prototype.$format = function() {
     // Do the replacing/formatting; args is now an object
    
     var result = this, match;
-		
-	for (var i = 0; match = /{(\d+|\w+)?}/gm.exec(result); i++) {
-	
-		var key = match[1];
-		
-		if (!key) {
-			result = result.replace("{}", args[i]);
-		}
-		else {
-			result = result.replace(new RegExp("\\{" + key + "\\}", "gm"), args[key]);
-		}
-	}
-	
+        
+    for (var i = 0; match = /{(\d+|\w+)?}/gm.exec(result); i++) {
+    
+        var key = match[1];
+        
+        if (!key) {
+            result = result.replace("{}", args[i]);
+        }
+        else {
+            result = result.replace(new RegExp("\\{" + key + "\\}", "gm"), args[key]);
+        }
+    }
+    
     return result;
-               
 };
