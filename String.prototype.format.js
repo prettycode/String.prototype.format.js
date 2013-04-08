73669555d662903d2556a72c233befbe4b5b9f47
Chris <accounts@prettycode.org>
2013-04-08 12:53:02 -0700

    Update String.prototype.format.js
    I don't care about licensing.

--- String.prototype.format.js
+++ String.prototype.format.js
@@ -2,10 +2,6 @@
     Original script title: "String.prototype.format.js"; version 1.0
     Copyright (c) 2011, Chris O'Brien, prettycode.org
     http://github.com/prettycode/String.prototype.format.js
-    
-    Permission is hereby granted for unrestricted use, modification, and redistribution of this
-    script, only under the condition that this code comment is kept wholly complete, appearing
-    directly above the script's code body, in all original or modified non-minified representations
 */ 
 
 String.prototype.format = function() {
