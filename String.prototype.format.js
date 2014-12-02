7d9270375c6d3039f8f1aec055f95824a4e69de6
Chris O. <chris@prettycode.org>
2014-12-02 08:59:33 -0800

    Update String.prototype.format.js
    Cleanup.

--- String.prototype.format.js
+++ String.prototype.format.js
@@ -1,36 +1,23 @@
 /*  
-    Original script title: "String.prototype.format.js"; version 1.0
     Copyright (c) 2011, Chris O'Brien, prettycode.org, MIT license
     http://github.com/prettycode/String.prototype.format.js
 */ 
-
 String.prototype.format = function() {
- 
-    // Convert `arguments` to real []
- 
-    var args = Array.prototype.slice.call(arguments);
-   
-    // First arg is an object map
+    var args = Array.prototype.slice.call(arguments),
+        format = this,
+        match;
    
     if (args.length === 1 && typeof args[0] === "object") {
         args = args[0];
     }
-   
-    // Do the replacing/formatting; args is now an object
-   
-    var result = this, match;
-        
-    for (var i = 0; match = /{(\d+|\w+)?}/gm.exec(result); i++) {
     
+    for (var i = 0; match = /{(\d+|\w+)?}/gm.exec(format); i++) {
         var key = match[1];
         
-        if (!key) {
-            result = result.replace("{}", args[i]);
-        }
-        else {
-            result = result.replace(new RegExp("\\{" + key + "\\}", "gm"), args[key]);
-        }
+        format = key ? 
+                 format.replace(new RegExp("\\{" + key + "\\}", "gm"), args[key]) :
+                 format.replace("{}", args[i]);
     }
     
-    return result;
+    return format;
 };
